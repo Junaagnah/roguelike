7a205ef16002b9c4b21e378e29721e35e934bf19
GEHRINGER Nicolas <nicolas.gehringer@outlook.fr>
2019-01-15 16:47:05 +0100

    Avancements récupération difficultés & enregistrement partie pour score

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score
+{
+    readonly int idUser;
+    readonly string pseudoUser;
+    readonly int scoreUser;
+    private int niveauPerso;
+    private int forcePerso;
+    private int argentPerso;
+    private int tours;
+    private int monstres;
+    private int boss;
+
+    public int IdUser { get => idUser; }
+    public string PseudoUser { get => pseudoUser; }
+    public int ScoreUser { get => scoreUser; }
+
+    public int NiveauPerso { get => niveauPerso; set => niveauPerso = value; }
+    public int ForcePerso { get => forcePerso; set => forcePerso = value; }
+    public int ArgentPerso { get => argentPerso; set => argentPerso = value; }
+    public int Tours { get => tours; set => tours = value; }
+    public int Monstres { get => monstres; set => monstres = value; }
+    public int Boss { get => boss; set => boss = value; }
+
+    public Score(int Id)
+    {
+        idUser = Id;
+    }
+
+    public Score(int IdUser, string PseudoUser, int Score)
+    {
+        idUser = IdUser;
+        pseudoUser = PseudoUser;
+        scoreUser = Score;
+    }
+
+    public void SaveGame(int niveau, int force, int argent, int tours, int monstres, int boss)
+    {
+        NiveauPerso = niveau;
+        ForcePerso = force;
+        ArgentPerso = argent;
+        Tours = tours;
+        Monstres = monstres;
+        Boss = boss;
+    }
+}
